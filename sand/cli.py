453369546683c8cf91f2616b90524a83427c618a
Nathan Capon <nathan.capon@hygeos.com>
2026-02-26 18:34:49 +0100

    Feat: Add cli to download product directly in terminal

--- sand/cli.py
+++ sand/cli.py
@@ -7,6 +7,7 @@ from sand.sample_product import products
 from sand._cli_cfg import SearchCfg
 
 from core import log
+from core.import_utils import import_module
 from core.ascii_table import ascii_table
 from core.table import read_csv
 from core.fuzzy import search
@@ -19,6 +20,20 @@ def entry():
         epilog="Made by HYGEOS."
     )
     subs = parser.add_subparsers(dest="command", required=True)
+    
+    
+    # > download command
+    dl = subs.add_parser(name="download", help="download product with SAND downloader")
+    dl.add_argument("--from", action="store", help="Provider to query")
+    dl.add_argument("--mute", action="store_true", help="Disable verbose")
+    dl.add_argument("--dir", action="store", help="Directory where to store downloaded product")
+    dl.add_argument(
+        "product_ids",
+        nargs="+",  # Captures ALL remaining arguments
+        help="Identifiers of product to download",
+        default=None
+    )
+    
     
     # > search command
     cmd = subs.add_parser(name="search", help="search products in the datasets interfaced by SAND")
@@ -52,11 +67,10 @@ def entry():
     
     cmd.add_argument("--nocolor", "-n", action="store_true", help="Disable color output", default=False)
     cmd.add_argument("--compact", "-c", action="store_true", help="Compact layout", default=False)
-    
-    
+        
     
     # > search command
-    cmd = subs.add_parser(name="sample", help="get a sample product identifier")
+    sample = subs.add_parser(name="sample", help="get a sample product identifier")
     
     args = parser.parse_args()
     
@@ -96,11 +110,38 @@ def entry():
         
         # Display final results
         if len(df) == 0:
-            print('> no result found.')
+            print(log.rgb.orange, '> No result found.')
             exit()
         
         df = df.sort_values(by='Name')
         ascii_table(df, colors=cfg.colors).print()
     
+    if args.command == 'download':
+        
+        # Initialize all provider downloaders
+        providers = {
+            'cdse': 'sand.copernicus_dataspace.DownloadCDSE',
+            'eumdac': 'sand.eumdac.DownloadEumDAC',
+            'cnes': 'sand.cnes.DownloadCNES',
+            'nasa': 'sand.nasa.DownloadNASA',
+            'usgs': 'sand.usgs.DownloadUSGS',
+        }
+        
+        # Select deserved downloader
+        if getattr(args, 'from') not in providers.keys():
+            print(log.rgb.red, f'> Provider is not part of {list(providers.keys())}')
+            exit()
+        Downloader = import_module(providers[getattr(args, 'from')])
+        
+        # Disable verbose messages
+        if args.mute:
+            log.set_lvl(log.lvl.WARNING)
+        
+        # Instantiate and query API
+        prod_ids, directory = list(args.product_ids), args.dir
+        for prod in prod_ids:
+            Downloader().download_file(prod, directory)
+            print(log.rgb.green, f'> {prod} successfully download.')
+    
     if args.command == 'sample':
-        print('> Implementation of sample command is on going.')
+        print(log.rgb.orange, '> Implementation of sample command is on going.')
